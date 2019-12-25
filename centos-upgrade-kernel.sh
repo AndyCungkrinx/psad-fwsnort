fdbffc109c2a581a72eb3d7f927f5280eaf233d8
unknown <andy@icube.us>
2019-12-25 14:51:12 +0700

    centos psad+fwsnort beta

---
+++ centos-upgrade-kernel.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo " ______                              _           ______                  _           _             
+(____  \              /\            | |         / _____)                | |         (_)            
+ ____)  )_   _ _     /  \  ____   _ | |_   _   | /     _   _ ____   ____| |  _  ____ _ ____  _   _ 
+|  __  (| | | (_)   / /\ \|  _ \ / || | | | |  | |    | | | |  _ \ / _  | | / )/ ___) |  _ \( \ / )
+| |__)  ) |_| |_   | |__| | | | ( (_| | |_| |  | \____| |_| | | | ( ( | | |< (| |   | | | | |) X ( 
+|______/ \__  (_)  |______|_| |_|\____|\__  |   \______)____|_| |_|\_|| |_| \_)_|   |_|_| |_(_/ \_)
+        (____/                        (____/                      (_____| "
+
+echo "=======================================================================================================
+Upgrading latest kernel
+======================================================================================================="
+rpm –-import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+rpm -Uvh https://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+yum update && \
+yum --enablerepo=elrepo-kernel install kernel-ml -y && \
+yum --enablerepo=elrepo-kernel install kernel-lt -y && \
+sed -i 's/GRUB_DEFAULT=saved/GRUB_DEFAULT=0/g' /etc/default/grub
+grub2-mkconfig -o /boot/grub2/grub.cfg
+grub2-set-default 0
+echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+Kernel has been upgraded wait for rebooting
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+reboot
